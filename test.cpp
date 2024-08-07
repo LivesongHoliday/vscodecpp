a3af6f767b452f10f4ff9f7daf33ef830d9e41cc
LivesongHoliday <ljylivesong@gmail.com>
2024-08-07 13:19:43 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    a = 100;
+    b = 200;
+
+    cout << "请输入数字：";
+    cin >> a;
+    cout << a << endl
+         << b << endl;
+    system("pause");
+
+    return 0;
+}
